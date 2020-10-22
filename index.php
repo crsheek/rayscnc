4074fb54e052e4934c3b4b2cc3ab944d71c9961e
Cindy Sheek <cindy@textusconsulting.com>
2020-10-22 14:58:37 -0700

    add page header

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
 
-
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
